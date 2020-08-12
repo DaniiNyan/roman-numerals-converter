77a61646bace0fe5c9026f2b69e8febcb378386d
DaniiNyan <daniela.araujo@ilegra.com>
2020-08-11 21:08:18 -0300

    add menu

--- src/main/java/com/daniinyan/converter/Application.java
+++ src/main/java/com/daniinyan/converter/Application.java
@@ -1,9 +1,36 @@
 package com.daniinyan.converter;
 
+import com.daniinyan.converter.service.Menu;
+import java.util.Scanner;
+
 public class Application {
 
   public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    Menu menu = new Menu();
 
+    boolean wantToContinue = true;
+
+    while (wantToContinue) {
+      menu.showOptions();
+      int selectedOption = keyboard.nextInt();
+
+      switch (selectedOption) {
+        case 1: {
+          menu.convertIntegerToRomanNumeral(keyboard);
+          break;
+        }
+        case 2: {
+          menu.convertRomanNumeralToInteger(keyboard);
+          break;
+        }
+
+        default: {
+          wantToContinue = false;
+          break;
+        }
+      }
+    }
+    menu.exit();
   }
-
 }
--- src/main/java/com/daniinyan/converter/service/Menu.java
+++ src/main/java/com/daniinyan/converter/service/Menu.java
@@ -1,5 +1,47 @@
 package com.daniinyan.converter.service;
 
+import com.daniinyan.converter.exception.ValueNotSupportedException;
+import java.util.Scanner;
+
 public class Menu {
 
+  private final Converter converter = new Converter();
+
+  public void showOptions() {
+    System.out.println("========================");
+    System.out.println("Available Options:");
+    System.out.println("1. Convert normal number to roman numeral;");
+    System.out.println("2. Convert roman numeral to normal number;");
+    System.out.println("========================");
+    System.out.println("Press the option you want or any other key to exit.");
+  }
+
+  public void convertIntegerToRomanNumeral(Scanner keyboard) {
+    System.out.println("You selected option 1, convert normal number to roman number.");
+    System.out.println("Type a normal number to be converted.");
+    int number = keyboard.nextInt();
+
+    try {
+      System.out.println("Result: " + converter.integerToRomanNumeral(number));
+    } catch (ValueNotSupportedException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public void convertRomanNumeralToInteger(Scanner keyboard) {
+    System.out.println("You selected option 2, convert roman numeral to normal number.");
+    System.out.println("Type a roman numeral to be converted.");
+    String romanNumeral = keyboard.next();
+
+    try {
+      System.out.println("Result: " + converter.romanNumeralToInteger(romanNumeral));
+    } catch (ValueNotSupportedException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public void exit() {
+    System.out.println("You exited.");
+    System.exit(0);
+  }
 }
